69e2a481adcb953c8488caa05a325b2afcad6d8e
Giacomo <mymail@myprovider.com>
2019-12-16 00:04:52 +0100

    day14 parsing

--- src/main/java/day14/TestMainDay14.java
+++ src/main/java/day14/TestMainDay14.java
@@ -6,6 +6,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -19,19 +20,30 @@ public class TestMainDay14 {
 	public static void main(String[] args) throws IOException {
 		Path path = FileSystems.getDefault().getPath(PUZZLE_INPUT);
 
-		Map<String, List<Element>> asteroidsBelt = loadMapOfReaction(path);
+		Map<String, Reaction> reactions = loadMapOfReaction(path);
+
+		System.out.println(reactions);
 
 	}
 
-	private static Map<String, List<Element>> loadMapOfReaction(Path path) throws IOException {
-		Map<String, List<Element>> reactions = new HashMap<>();
+	private static Map<String, Reaction> loadMapOfReaction(Path path) throws IOException {
+		Map<String, Reaction> reactions = new HashMap<>();
 
 		Iterator<String> it = Files.lines(path, StandardCharsets.UTF_8).iterator();
 
 		while (it.hasNext()) {
 			String line = it.next();
 
-			String[] sep = line.split("=>");
+			String[] sep = line.replace("=> ", "=>").split("=>");
+			String[] result = sep[1].split(" ");
+			Reaction curr = new Reaction(Integer.parseInt(result[0].trim()));
+
+			String[] elements = sep[0].replace(", ", ",").split(",");
+			for (int i = 0; i < elements.length; i++) {
+				String[] token = elements[i].split(" ");
+				curr.requisite.add(new Element(Integer.parseInt(token[0].trim()), token[1].trim()));
+			}
+			reactions.put(result[1].trim(), curr);
 		}
 
 		return reactions;
@@ -45,5 +57,23 @@ class Element {
 	public Element(int qty, String name) {
 		this.qty = qty;
 		this.name = name;
+	}
+
+	public String toString() {
+		return "<" + qty + " " + name + ">";
+	}
+}
+
+class Reaction {
+	int qtyProd;
+	List<Element> requisite;
+
+	public Reaction(int qtyProd) {
+		this.qtyProd = qtyProd;
+		requisite = new ArrayList<>();
+	}
+
+	public String toString() {
+		return "Prod:" + qtyProd + " " + requisite;
 	}
 }
